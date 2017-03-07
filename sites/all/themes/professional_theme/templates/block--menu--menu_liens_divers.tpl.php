aefa1d7423aed170e4c9c8ff1f15bfa0a42e8efd
Hairidine <hairidineabdousmi@yahoo.fr>
2017-03-07 20:55:20 +0100

    MAJ devel

--- sites/all/themes/professional_theme/templates/block--menu--menu_liens_divers.tpl.php
+++ sites/all/themes/professional_theme/templates/block--menu--menu_liens_divers.tpl.php
@@ -41,15 +41,15 @@
  */
 ?>
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php print "Je suis le nouveau template !"; ?>
   <?php print render($title_prefix); ?>
   <?php if (!empty($block->subject)): ?>
     <h2 <?php print $title_attributes; ?>><?php print $block->subject ?></h2>
   <?php endif;?>
+    <?php print "Je suis le nouveau template !"; ?>
   <?php print render($title_suffix); ?>
 
   <div class="content"<?php print $content_attributes; ?>>
     <?php print $content ?>
   </div>
-  
+
 </div> <!-- /.block -->
--- sites/all/themes/professional_theme/templates/region.tpl.php
+++ sites/all/themes/professional_theme/templates/region.tpl.php
@@ -1,5 +1,6 @@
 <?php if (!empty($content)): ?>
 <div class="<?php print $classes; ?>">
+    <div class="warning"><?php print $region; ?></div>
   <?php print $content; ?>
 </div>
 <?php endif; ?> <!-- /.region -->
